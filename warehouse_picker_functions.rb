719b1c62e1f033764cd0f80208091b2dc5050251
Wojtek Tartanus <wtartanus@gmail.com>
2016-04-29 11:44:21 +0100

    Updated data structure

--- warehouse_picker_functions.rb
+++ warehouse_picker_functions.rb
@@ -2,3 +2,41 @@
  #First array values should be a10 .... a1 
  #Second array values shoud be c1 ... c10
  #Third arrat values shoud be b1 ... b10
+
+  WAREHOUSE_BAY = [
+    { shelf: "a10", item: "rubber band" },
+    { shelf: "a9", item: "glow stick" },
+    { shelf: "a8", item: "model car" },
+    { shelf: "a7", item: "bookmark" },
+    { shelf: "a6", item: "shovel" },
+    { shelf: "a5", item: "rubber duck" },
+    { shelf: "a4", item: "hanger" },
+    { shelf: "a3", item: "blouse" },
+    { shelf: "a2", item: "stop sign" },
+    { shelf: "a1", item: "needle" },
+
+    { shelf: "c1", item: "rusty nail" },
+    { shelf: "c2", item: "drill press" },
+    { shelf: "c3", item: "chalk" },
+    { shelf: "c4", item: "word search" },
+    { shelf: "c5", item: "thermometer" },
+    { shelf: "c6", item: "face wash" },
+    { shelf: "c7", item: "paint brush" },
+    { shelf: "c8", item: "candy wrapper" },
+    { shelf: "c9", item: "shoe lace" },
+    { shelf: "c10", item: "leg warmers" },
+
+    { shelf: "b1", item: "tyre swing" },
+    { shelf: "b2", item: "sharpie" },
+    { shelf: "b3", item: "picture frame" },
+    { shelf: "b4", item: "photo album" },
+    { shelf: "b5", item: "nail filer" },
+    { shelf: "b6", item: "tooth paste" },
+    { shelf: "b7", item: "bath fizzers" },
+    { shelf: "b8", item: "tissue box" },
+    { shelf: "b9", item: "deodorant" },
+    { shelf: "b10", item: "cookie jar" },
+
+
+
+  ]
